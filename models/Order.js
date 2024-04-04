7a207df3b3747cd6e3aa61c7b44812de746c5510
janagagan <janagagan@gmail.com>
2024-04-04 20:04:29 +0800

    Updated Order model

--- models/Order.js
+++ models/Order.js
@@ -11,6 +11,10 @@ const orderSchema = new mongoose.Schema({
                 type: String,
                 required: [true, 'Product ID is Required']
             },
+            productName: {
+                type: String,
+                required: [true, 'Product Name is Required']
+            },
             quantity: {
                 type: Number,
                 required: [true, 'Quantity is Required']
